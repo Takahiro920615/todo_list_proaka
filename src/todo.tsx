dbec2259f1c8d290e1093a175d11bec441a5e386
stproake <test@example.com>
2025-01-24 18:14:16 +0900

    reactチュートリアル２終了

--- src/todo.tsx
+++ src/todo.tsx
@@ -21,11 +21,37 @@ const Todo: React.FC = () => {
     content: text,
     id: nexId,
   };
+
   
   setTodos((prevTodos) => [newTodo, ...prevTodos]);
   setNextId(nexId + 1)
   setText('');
 };
+
+const handleEdit = (id: number, value: string) => {
+  setTodos((todos) => {
+    /**
+     * 引数として渡された todo の id が一致する
+     * 更新前の todos ステート内の todo の
+     * value プロパティを引数 value (= e.target.value) に書き換える
+     */
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, content: value};
+      }
+      return todo;
+    });
+
+    console.log('=== Original todos ===');
+    todos.map((todo) => {
+      console.log(`id: ${todo.id}, content: ${todo.content}`);
+    });
+    
+    // todos ステートを更新
+    return newTodos;
+  });
+};
+
 
   return(
    <div>
@@ -44,18 +70,17 @@ const Todo: React.FC = () => {
     <input type="submit" content="追加"/>
     </form>
     <ul>
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <input
-                type="text"
-                value={todo.content}
-                onChange={(e) => e.preventDefault()}
-              />
-            </li>
-        )}
-          
-        )}
+      { todos.map((todo) =>{
+        return (
+          <li key={todo.id}>
+            <input
+              type="text"
+              value={todo.content}
+              onChange={(e) => handleEdit(todo.id, e.target.value)}
+            />
+          </li>
+        );
+      })}
     </ul>
    </div> 
   );
